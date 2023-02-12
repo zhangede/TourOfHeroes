7da0373f5d75977ee30f5e095f5a8f7c880c1e01
Feng <zhang7777@gmail.com>
2023-02-12 14:29:52 +0100

    Init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
