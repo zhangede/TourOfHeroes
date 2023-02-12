1684913d7b0e7ec1989e856a9af98f85c5c20d8b
Feng <zhang7777@gmail.com>
2023-02-12 15:46:57 +0100

    add service

---
+++ src/app/hero.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { MessagesService } from './messages.service';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroService {
+  constructor(private messageService: MessagesService) {}
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
+  }
+}
